2a64f6bf6f45c4a8e4067682f674a73e0b97864d
Rodrigo <rodrigofcannabrava@gmail.com>
2025-09-25 14:17:42 -0700

    core: add eslit sort plugin and prettier tailwind plugin

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,10 +3,10 @@ import { Input } from "@/components/ui/input";
 
 const ProductPage = () => {
   return (
-    <div className="p-5 border border-red-500 rounded-xl">
+    <div className="rounded-xl border border-red-500 p-5">
       <h1 className="text-red-500">Prodructs page</h1>
       <Button>Mc Donalds</Button>
-      <Input placeholder="Fazer pedido"/>
+      <Input placeholder="Fazer pedido" />
     </div>
   );
 };
